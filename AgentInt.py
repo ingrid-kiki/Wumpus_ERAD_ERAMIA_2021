692c2dcc056643089bad2cc080c237417d19ddf0
Ingrid Mendes <45125081+ingrid-kiki@users.noreply.github.com>
2025-07-03 04:12:26 -0300

    Update AgentInt.py
    adição de comentários para facilitar a compreesão do código

--- AgentInt.py
+++ AgentInt.py
@@ -1,9 +1,12 @@
 import Action
 
+# Classe base para todos os agentes do ambiente Wumpus
 class AgentInt(object):
 
     def __init__(self):
+        # Construtor da classe base, pode ser expandido por subclasses
         pass
 
-    def process(self,percept) -> Action:
+    # Método que deve ser implementado pelas subclasses para processar percepções do ambiente
+    def process(self, percept) -> Action:
         pass
